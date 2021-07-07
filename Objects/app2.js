e92d2ee6ef982dfa92a0eff0154de64c024f3a11
noreen <noreenakhtar922@gmail.com>
2021-07-08 00:08:59 +0500

    object values changed

---
+++ Objects/app2.js
@@ -0,0 +1,15 @@
+// 2. Now, using the same object,
+//  change the userName to your name, fatherName to your father’s name, and className to your class.
+
+let userObject = {
+    userName: "Philip",
+    fatherName: "Norman",
+    className: "four",
+};
+
+userObject.userName="Noreen";
+userObject.fatherName="Noor Ahmad";
+userObject.className="Graduate";
+console.log(userObject.userName);
+console.log(userObject.fatherName);
+console.log(userObject.className);
