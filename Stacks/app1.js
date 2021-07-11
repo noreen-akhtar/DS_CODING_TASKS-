df67d24e5268b503a083a284c620bacc550be2dc
noreen <noreenakhtar922@gmail.com>
2021-07-11 10:48:18 +0500

    SKACK OVERFLOW AND QUEUING AND DEQUEUING

--- Stacks/app1.js
+++ Stacks/app1.js
@@ -1,9 +1,21 @@
-//Implement Stack using an array. Initialize it with an array which will be used to store values and a size property that will be used to track the size of stack.
+//Implement Stack using an array. Initialize it with an array which will be used to store values 
+// and a size property that will be used to track the size of stack.
 //stack class
-class Stack {
-    //// Array is used to implement stack
-    constructor() {
-      this.items = [];
-      this.size = 0;
-    }
-}
+let stack = [];
+
+stack.push(1);
+console.log(stack); // [1]
+
+stack.push(2);
+console.log(stack); // [1,2]
+
+stack.push(3);
+console.log(stack); // [1,2,3]
+
+stack.push(4);
+console.log(stack); // [1,2,3,4]
+
+stack.push(5);
+console.log(stack); // [1,2,3,4,5]
+
+this.size = 0;
